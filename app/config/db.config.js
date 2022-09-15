2e74bf200a9d63e60f978f4b07c2235c36d1d2e1
nshiva20 <112461911+nshiva20@users.noreply.github.com>
2022-09-15 18:12:42 +0530

    API for save the details

---
+++ app/config/db.config.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//   url: "mongodb://localhost:27017/Upskilling"
+// };
+
+// const { MongoClient, ServerApiVersion } = require('mongoose');
+// const uri = "mongodb+srv://nshiva:FfGqjk70KFfxyzk1@cluster1.mlnqlod.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true, useUnifiedTopology: true,
+//   serverApi: ServerApiVersion.v1
+// });
+// client.connect(err => {
+//   const collection = client.db("Upskilling").collection("user_data");
+//   // perform actions on the collection object
+//   client.close();
+// });
